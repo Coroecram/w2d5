29c8fb0540db00ddfa762e41defbbcbe678fea22
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-09 15:01:50 -0400

    Start linked list p04

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -23,21 +23,49 @@ class LinkedList
   end
 
   def first
+    @head
   end
 
   def last
+    return @head if  @head.next.nil?
+    link = @head
+    until link.next.nil?
+      link = link.next
+    end
+
+    link
   end
 
   def empty?
+    @head.key.nil?
   end
 
   def get(key)
+    link = @head
+    until link.nil?
+      return link.value if link.key == key
+      link = link.next
+    end
+
+    nil
   end
 
   def include?(key)
+    link = @head
+    until link.nil?
+      return true if link.key == key
+      link = link.next
+    end
+
+    false
   end
 
   def insert(key, val)
+    if empty?
+      @head = Link.new(key, val)
+    else
+      last.next = Link.new(key, val)
+    end
   end
 
   def remove(key)
